9d7da3223358e71d1acd3b12b12db601a746fb6a
unknown <vic_ortiz20@hotmail.es>
2021-09-03 09:12:28 -0500

    Rama api

--- front/src/app/shared/directives/loading-btn.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoadingBtnDirective } from './loading-btn.directive';
-
-describe('LoadingBtnDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoadingBtnDirective();
-    expect(directive).toBeTruthy();
-  });
-});
